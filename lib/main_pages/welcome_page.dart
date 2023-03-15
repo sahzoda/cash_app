057fb78b2555c7950e5c9095e89e86ba62e98d0f
shahzoda <shahzodanigmatjanova524@gmail.com>
2023-03-15 21:45:41 +0600

    cash_app started

---
+++ lib/main_pages/welcome_page.dart
@@ -0,0 +1,135 @@
+import 'package:cash_app/main_pages/log_in.dart';
+import 'package:flutter/material.dart';
+
+import '../welcome_widgets/welcome_widget.dart';
+
+
+class welcomePage extends StatefulWidget {
+  const welcomePage({Key? key}) : super(key: key);
+
+  @override
+  _welcomePageState createState() => _welcomePageState();
+}
+
+class _welcomePageState extends State<welcomePage> {
+  late String email;
+  late String password;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+      child: 
+              // SizedBox(
+              //   height: 120,
+                
+              // ),
+             
+              Column(
+               mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(30),
+                          bottomRight: Radius.circular(30),
+                          topLeft: Radius.circular(30),
+                          bottomLeft: Radius.circular(30),
+                        ),
+                       
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          LogSignWidget(
+                            title: 'Admin',
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder:(context) => SignUpPage(),));
+                            },
+                          ),
+                       
+                          SizedBox(
+                            height: 10,
+                           
+                          ),
+                        ],
+                      ),
+                    ),
+            
+                  // Container(
+                  //   width: 60,
+                  //   decoration: BoxDecoration(
+                     
+                  //     borderRadius: BorderRadius.only(
+                  //       topLeft: Radius.circular(30),
+                  //       bottomLeft: Radius.circular(30),
+                  //     ),
+                  //   ),
+                    
+                  // ),
+                  // SizedBox(
+                  //   height: 6,
+                  // ),
+                  // Container(
+                  
+                  //   decoration: BoxDecoration(
+                    
+                  //     borderRadius: BorderRadius.only(
+                  //       topLeft: Radius.circular(30),
+                  //       bottomLeft: Radius.circular(30),
+                  //     ),
+                  //   ),
+                  //   child: Padding(
+                  //     padding: EdgeInsets.symmetric(vertical: 0.01),
+                  
+                  //       child: Row(
+                  //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  //         children: [
+                  //           // SizedBox(
+                  //           //   height: 45,
+                              
+                  //           // ),
+                  //           LogSignWidget(
+                  //             title: 'Log In', 
+                  //             onTap: () {}
+                  //             ),
+                         
+                  //         ],
+                  //       )
+                  //       ),
+                  // ),
+                 
+                   Container(
+                     decoration: BoxDecoration(
+                       borderRadius: BorderRadius.only(
+                         topRight: Radius.circular(30),
+                         bottomRight: Radius.circular(30),
+                         topLeft: Radius.circular(30),
+                         bottomLeft: Radius.circular(30),
+                       ),
+                      
+                     ),
+                     child: Padding(
+                         padding: const EdgeInsets.all(50.0),
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.center,
+                           children: [
+                             LogSignWidget(
+                               title: 'Teacher',
+                               onTap: () {
+                                    Navigator.push(context, MaterialPageRoute(builder:(context) => SignUpPage(),));
+                               },
+                             ),
+                          
+                             SizedBox(
+                               height: 45,
+                              
+                             ),
+                           ],
+                         )),
+                   ),
+                ],
+              ),
+              ),
+        );
+  }
+}
