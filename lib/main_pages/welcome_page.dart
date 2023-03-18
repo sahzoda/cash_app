8a0995b467e168dd4f1698274f0d546b8a41507e
shahzoda <shahzodanigmatjanova524@gmail.com>
2023-03-18 20:39:21 +0600

    Cash_app

--- lib/main_pages/welcome_page.dart
+++ lib/main_pages/welcome_page.dart
@@ -17,119 +17,124 @@ class _welcomePageState extends State<welcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-      child: 
-              // SizedBox(
-              //   height: 120,
-                
-              // ),
+      body: Stack(
+      
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
              
-              Column(
-               mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(30),
-                          bottomRight: Radius.circular(30),
-                          topLeft: Radius.circular(30),
-                          bottomLeft: Radius.circular(30),
-                        ),
-                       
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          LogSignWidget(
-                            title: 'Admin',
-                            onTap: () {
-                              Navigator.push(context, MaterialPageRoute(builder:(context) => SignUpPage(),));
-                            },
-                          ),
-                       
-                          SizedBox(
-                            height: 10,
-                           
-                          ),
-                        ],
-                      ),
-                    ),
-            
-                  // Container(
-                  //   width: 60,
-                  //   decoration: BoxDecoration(
-                     
-                  //     borderRadius: BorderRadius.only(
-                  //       topLeft: Radius.circular(30),
-                  //       bottomLeft: Radius.circular(30),
-                  //     ),
-                  //   ),
-                    
-                  // ),
-                  // SizedBox(
-                  //   height: 6,
-                  // ),
-                  // Container(
-                  
-                  //   decoration: BoxDecoration(
-                    
-                  //     borderRadius: BorderRadius.only(
-                  //       topLeft: Radius.circular(30),
-                  //       bottomLeft: Radius.circular(30),
-                  //     ),
-                  //   ),
-                  //   child: Padding(
-                  //     padding: EdgeInsets.symmetric(vertical: 0.01),
-                  
-                  //       child: Row(
-                  //         mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  //         children: [
-                  //           // SizedBox(
-                  //           //   height: 45,
-                              
-                  //           // ),
-                  //           LogSignWidget(
-                  //             title: 'Log In', 
-                  //             onTap: () {}
-                  //             ),
-                         
-                  //         ],
-                  //       )
-                  //       ),
-                  // ),
+              
+              child: Image.asset("assets/images/photo.png" ,)),
+          ),
+          Column(
+         mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                    topLeft: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                  ),
                  
-                   Container(
-                     decoration: BoxDecoration(
-                       borderRadius: BorderRadius.only(
-                         topRight: Radius.circular(30),
-                         bottomRight: Radius.circular(30),
-                         topLeft: Radius.circular(30),
-                         bottomLeft: Radius.circular(30),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    LogSignWidget(
+                      title: 'Admin',
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder:(context) => SignUpPage(),));
+                      },
+                    ),
+                 
+                    SizedBox(
+                      height: 10,
+                     
+                    ),
+                  ],
+                ),
+              ),
+              
+            // Container(
+            //   width: 60,
+            //   decoration: BoxDecoration(
+               
+            //     borderRadius: BorderRadius.only(
+            //       topLeft: Radius.circular(30),
+            //       bottomLeft: Radius.circular(30),
+            //     ),
+            //   ),
+              
+            // ),
+            // SizedBox(
+            //   height: 6,
+            // ),
+            // Container(
+            
+            //   decoration: BoxDecoration(
+              
+            //     borderRadius: BorderRadius.only(
+            //       topLeft: Radius.circular(30),
+            //       bottomLeft: Radius.circular(30),
+            //     ),
+            //   ),
+            //   child: Padding(
+            //     padding: EdgeInsets.symmetric(vertical: 0.01),
+            
+            //       child: Row(
+            //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //         children: [
+            //           // SizedBox(
+            //           //   height: 45,
+                        
+            //           // ),
+            //           LogSignWidget(
+            //             title: 'Log In', 
+            //             onTap: () {}
+            //             ),
+                   
+            //         ],
+            //       )
+            //       ),
+            // ),
+           
+             Container(
+               decoration: BoxDecoration(
+                 borderRadius: BorderRadius.only(
+                   topRight: Radius.circular(30),
+                   bottomRight: Radius.circular(30),
+                   topLeft: Radius.circular(30),
+                   bottomLeft: Radius.circular(30),
+                 ),
+                
+               ),
+               child: Padding(
+                   padding: const EdgeInsets.all(50.0),
+                   child: Row(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: [
+                       LogSignWidget(
+                         title: 'Teacher',
+                         onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder:(context) => SignUpPage(),));
+                         },
                        ),
-                      
-                     ),
-                     child: Padding(
-                         padding: const EdgeInsets.all(50.0),
-                         child: Row(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           children: [
-                             LogSignWidget(
-                               title: 'Teacher',
-                               onTap: () {
-                                    Navigator.push(context, MaterialPageRoute(builder:(context) => SignUpPage(),));
-                               },
-                             ),
-                          
-                             SizedBox(
-                               height: 45,
-                              
-                             ),
-                           ],
-                         )),
-                   ),
-                ],
-              ),
-              ),
+                    
+                       SizedBox(
+                         height: 45,
+                        
+                       ),
+                     ],
+                   )),
+             ),
+          ],
+        ),
+        ],
+      
+      ),
         );
   }
 }
