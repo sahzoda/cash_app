3ac98392037d6590eedd98318dc69c820b3b71c8
shahzoda <shahzodanigmatjanova524@gmail.com>
2023-04-01 21:49:12 +0600

    гмприлтоь

--- lib/main_pages/log_in.dart
+++ lib/main_pages/log_in.dart
@@ -46,97 +46,103 @@ class _SignUpPageState extends State<SignUpPage> {
     return ScaffoldMessenger(
         key: _scaffoldKey,
         child: Scaffold(
-          // backgroundColor: Color(),
           body: SafeArea(
             child: Form(
               key: _formKey,
-              child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Image.asset(
-                        "assets/images/photo.png",
-                        height: 200,
-                      ),
+              child: Column(children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 100),
+                    child: Image.asset(
+                      "assets/images/photo.png",
+                      height: 200,
                     ),
-                    SizedBox(
-                      height: 85,
-                    ),
-                    Column(mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                      // Text(
-                      //   "Введите ваш Логин ",
-                      //   style: TextStyle(
-                      //       fontWeight: FontWeight.w900, fontSize: 20),
-                      // ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20, left: 20),
-                        child: Container(
-                          width: 400,
-                          height: 45,
-                          decoration: BoxDecoration(
-                              color: Colors.grey.shade200,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Text(
-                            'Логин',
-                            style: TextStyle(
-                                fontSize: 20, color: Colors.grey.shade800),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20, left: 20),
-                        child: Container(
-                          width: 400,
-                          height: 45,
-                          decoration: BoxDecoration(
-                              color: Colors.grey.shade200,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Row(
-                            children: [
-                              Text(
-                                'Пароль',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.grey.shade800),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 100,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.grey.shade800,
+                  ),
+                ),
+                SizedBox(
+            height: 90,
+                ),
+                // Padding(
+                //   padding: const EdgeInsets.only(left: 5),
+                //   child: Text(
+                //     "Введите ваш персональный Логин ",
+                //     style: TextStyle(
+                //         fontWeight: FontWeight.w900, fontSize: 20),
+                //   ),
+                // ),
+                // SizedBox(
+                //   height: 20,
+                // ),
+                // Padding(
+                //   padding: const EdgeInsets.only(right: 20, left: 20),
+                //   child: Container(
+                //     width: 400,
+                //     height: 45,
+                //     decoration: BoxDecoration(
+                //         color: Colors.grey.shade300,
+                //         borderRadius: BorderRadius.circular(15)),
+                //     child: Row(
+                //       children: [
+                //         Text(
+                //           'Логин',
+                //           style: TextStyle(
+                //               fontSize: 20, color: Colors.grey.shade800),
+                //         ),
+                //       ],
+                //     ),
+                //   ),
+                // ),
 
-                          // border:Border.all(color:Colors.red)
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 150,
-                            vertical: 10,
-                          ),
-                          child: Text(
-                            'Продолжить',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(13)),
+                      labelText: 'Логин',
+                    ),
+                  ),
+                ),
+
+              
+                 Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(13)),
+                      labelText: 'Пароль',
+                    ),
+                  ),
+                ),
+
+                
+                SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color(0xff535353),
+
+                    // border:Border.all(color:Colors.red)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 150,
+                      vertical: 10,
+                    ),
+                    child: Text(
+                      'Продолжить',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ]),
-                  ]),
+                    ),
+                  ),
+                ),
+              ]),
             ),
           ),
         ));
